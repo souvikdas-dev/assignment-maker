1ebda9a3383bf70c801043dc1cdb39a7026e081c
SouvikDas-git <sd8338820@gmail.com>
2021-09-28 18:37:14 +0530

    Commit Changes

---
+++ js/controls.js
@@ -0,0 +1,145 @@
+let UploadPage = document.getElementById("UploadPage");
+let DefaultPage = document.getElementById("DefaultPage");
+let ChangeFont = document.getElementById("ChangeFont");
+let UploadFont = document.getElementById("UploadFont");
+let DownloadPage = document.getElementById("DownloadPage");
+
+let adjustX = document.getElementById("adjustX");
+let adjustY = document.getElementById("adjustY");
+let adjfontSize = document.getElementById("adjfontSize");
+let adjustWidth = document.getElementById("adjustWidth");
+
+let adjLetterSpacing = document.getElementById("adjLetterSpacing");
+let adjWordSpacing = document.getElementById("adjWordSpacing");
+let adjLineHeight = document.getElementById("adjLineHeight");
+
+UploadPage.onclick = () => {
+  var uploadPageBtn = document.createElement("input");
+  uploadPageBtn.type = "file";
+  uploadPageBtn.accept = "image/*";
+  // uploadPageBtn.style.display = "none";
+  uploadPageBtn.click();
+  // input.remove();
+  console.log(uploadPageBtn);
+
+  uploadPageBtn.onchange = (evt) => {
+    const [file] = uploadPageBtn.files;
+    if (file) {
+      imgSrc = URL.createObjectURL(file);
+      console.log(imgSrc);
+      redraw();
+    }
+  };
+
+  //   uploadPageBtn.onchange = function(event) {
+  //     var reader = new FileReader();
+  //     reader.onload = function(){
+  //       var output = document.getElementById('blah');
+  //       output.src = reader.result;
+  //       imgSrc = reader.result;
+  //       redraw();
+  //     };
+  //     reader.readAsDataURL(event.target.files[0]);
+  //   };
+};
+
+DefaultPage.onclick = () => {
+  imgSrc = "pages/page (0).jpg";
+  redraw();
+};
+
+ChangeFont.onclick = () => {
+  fontindex < totalfontnum ? fontindex++ : (fontindex = 0);
+  loadFonts();
+  //   redraw();
+};
+
+UploadFont.onclick = () => {
+  var uploadFontBtn = document.createElement("input");
+  uploadFontBtn.type = "file";
+  // input.accept = "image/*";
+  //   input.style.display = "none";
+  uploadFontBtn.click();
+  // input.remove();
+  console.log("UploadFont clicked");
+
+  uploadFontBtn.onchange = (evt) => {
+    const file = uploadFontBtn.files[0];
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      async function () {
+        const font = new FontFace("myfont", "url(" + reader.result + ")");
+        console.log(font);
+        // wait for font to be loaded
+        await font.load();
+        // add font to document
+        document.fonts.add(font);
+        // enable font with CSS class
+        // document.body.classList.add("fonts-loaded");
+        ctx.font = dfont;
+        console.log(ctx.font);
+        ctx.fillStyle = "#00f";
+        redraw();
+      },
+      false
+    );
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+};
+
+DownloadPage.onclick = () => {
+  var link = document.createElement("a");
+  link.download = "page.png";
+  link.href = canvas.toDataURL("Image/png");
+  link.click();
+  link.remove();
+};
+
+adjustX.oninput = () => {
+  x = parseInt(adjustX.value);
+  redraw();
+};
+
+adjustY.oninput = () => {
+  y = parseInt(adjustY.value);
+  redraw();
+};
+
+adjfontSize.oninput = () => {
+  // var fs = parseInt(adjfontSize.value);
+  var fs = adjfontSize.value;
+  dfont = fs + "px myfont";
+  ctx.font = dfont;
+  redraw();
+};
+
+adjustWidth.oninput = () => {
+  maxWidth = parseFloat(adjustWidth.value);
+  redraw();
+};
+
+adjLetterSpacing.oninput = () => {
+  canvas.style.letterSpacing = adjLetterSpacing.value + "px";
+  console.log(canvas.style.letterSpacing);
+  ctx = canvas.getContext("2d");
+  ctx.font = dfont;
+  redraw();
+  console.log("adjadjLetterSpacing");
+};
+
+adjWordSpacing.oninput = () => {
+  canvas.style.wordSpacing = adjWordSpacing.value + "px";
+  console.log(canvas.style.wordSpacing);
+  ctx = canvas.getContext("2d");
+  ctx.font = dfont;
+  redraw();
+};
+
+adjLineHeight.oninput = () => {
+  lineHeight = parseFloat(adjLineHeight.value);
+  redraw();
+};
---
+++ js/script.js
@@ -0,0 +1,104 @@
+let canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var imgSrc = "pages/page (0).jpg";
+var dfont = "22px myfont";
+var mydata =
+  "What is Lorem Ipsum?\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\nWhy do we use it?\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+var maxWidth = 720;
+var lineHeight = 24;
+var x = 28;
+var y = 22 + 22;
+var fontindex = 0;
+var totalfontnum = 8;
+window.onload = draw();
+
+let text_input = document.getElementById("inputText");
+text_input.addEventListener("input", () => {
+//   console.log(text_input.value);
+  mydata = text_input.value;
+  redraw();
+});
+
+function draw() {
+  imageObj = loadimg();
+  imageObj.onload = function () {
+    ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+    defaultFontload();
+  };
+}
+function redraw() {
+  imageObj = loadimg();
+  imageObj.onload = function () {
+    ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+    drawText(ctx, mydata, x, y, maxWidth, lineHeight);
+  };
+}
+function loadimg() {
+  var imageObj = new Image();
+  imageObj.src = imgSrc;
+  return imageObj;
+}
+
+function drawText(context, text, x, y, line_width, line_height) {
+  var line = "";
+  var paragraphs = text.split("\n");
+  for (var i = 0; i < paragraphs.length; i++) {
+    var words = paragraphs[i].split(" ");
+    for (var n = 0; n < words.length; n++) {
+      var testLine = line + words[n] + " ";
+      var metrics = context.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > line_width && n > 0) {
+        context.fillText(line, x, y);
+        line = words[n] + " ";
+        y += line_height;
+      } else {
+        line = testLine;
+      }
+    }
+    context.fillText(line, x, y);
+    y += line_height;
+    line = "";
+  }
+}
+
+async function loadFonts() {
+  console.log(fontindex);
+  const font = new FontFace(
+    "myfont",
+    "url('fonts/font (" + fontindex + ").ttf')"
+  );
+  console.log(font);
+  // wait for font to be loaded
+  await font.load();
+  // add font to document
+  document.fonts.add(font);
+  // enable font with CSS class
+  // document.body.classList.add("fonts-loaded");
+  ctx.font = dfont;
+  console.log(ctx.font);
+  ctx.fillStyle = "#00f";
+  //   drawText(ctx, mydata, x, y, maxWidth, lineHeight);
+  redraw();
+
+  console.log("font loaded");
+  console.log(canvas.width);
+  console.log(canvas.height);
+  //   console.log(i);
+}
+async function defaultFontload() {
+  const font = new FontFace("myfont", "url('fonts/font (0).ttf')");
+  console.log(font);
+  // wait for font to be loaded
+  await font.load();
+  // add font to document
+  document.fonts.add(font);
+  // enable font with CSS class
+  // document.body.classList.add("fonts-loaded");
+  ctx.font = dfont;
+  console.log(ctx.font);
+  ctx.fillStyle = "#00f";
+  drawText(ctx, mydata, x, y, maxWidth, lineHeight);
+
+  console.log("default font loaded");
+}
