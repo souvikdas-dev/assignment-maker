4a4e99cdca4a22c30dd3172cfe1240278eb80a62
SouvikDas-git <sd8338820@gmail.com>
2021-10-08 13:59:51 +0530

    Commit Changes

---
+++ js/extend.js
@@ -0,0 +1,8 @@
+function openInGithub()
+{
+    var a = document.createElement("a");
+    a.href = "https://github.com/SouvikDas-git/AssignmentMaker";
+    a.target = "_blank";
+    a.click();
+    a.remove();
+}
